9f31cce76fcd76af5aafc3f246c6987e1c2bc95f
Luke Bonde <bondeluke@gmail.com>
2024-01-10 16:40:42 -0600

    This is definitely commit worthy

--- app/src/main/java/com/example/embody/MainActivity.kt
+++ app/src/main/java/com/example/embody/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -43,28 +42,19 @@ fun CirclesAndStuff(modifier: Modifier = Modifier) {
             .fillMaxSize()
             .background(Color.White)
     ) {
-        val neck = getNecklaces(6)
         drawAxes()
-        drawNecklace(
-            Point(center.x, size.height / 4),
-            3 * size.width / 8f,
-            listOf(
-                true, false, true, false,
-                true, true, false, true,
-                false
-            ),
-            30
-        )
-        drawNecklace(
-            Point(center.x, 3 * size.height / 4),
-            3 * size.width / 8f,
-            listOf(
-                true, false, true, false,
-                true, true, false, true,
-                false, true, false, true,
-                false, true, false, true,
+
+        val necklaces = getNecklaces(6).map { it.key }.groupBy { it.elements.size }
+        val numLevels = necklaces.size
+        val increments = 2 * numLevels
+        val padding = (size.height / numLevels) / 5
+        for (increment in (1..<increments step 2)) {
+            drawNecklace(
+                center = Point(center.x, increment * size.height / increments),
+                radius = size.height / (numLevels * 2) - padding,
+                values = listOf(true, false, true, false, true, true)
             )
-        )
+        }
     }
 }
 
--- app/src/main/java/com/example/embody/graphics/drawCircles.kt
+++ app/src/main/java/com/example/embody/graphics/drawCircles.kt
@@ -3,26 +3,31 @@ package com.example.embody.graphics
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.unit.dp
 import kotlin.math.cos
 import kotlin.math.sin
 
 internal fun DrawScope.drawNecklace(
-    cofCenter: Point,
-    cofRadius: Float,
-    values: List<Boolean>,
-    cr: Int = 20
+    center: Point,
+    radius: Float,
+    values: List<Boolean>
 ) {
     val numCircles = values.size
 
+    drawCircle(
+        color = Color.Black,
+        radius = 10f,
+        center = Offset(center.x, center.y),
+        alpha = 1f
+    )
+
     for (index in (0 until numCircles)) {
         val angle = index / numCircles.toFloat() * 2f * Math.PI - Math.PI / 2 // radians
-        val x = cofCenter.x + cos(angle).toFloat() * cofRadius
-        val y = cofCenter.y + sin(angle).toFloat() * cofRadius
+        val x = center.x + cos(angle).toFloat() * radius
+        val y = center.y + sin(angle).toFloat() * radius
 
         drawCircle(
             color = Color.Black,
-            radius = cr.dp.toPx(),
+            radius = radius / 4,
             center = Offset(x, y),
             alpha = if (values[index]) 1.0f else 0.5f
         )
